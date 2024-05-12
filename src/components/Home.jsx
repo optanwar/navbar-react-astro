4ee62151375f127a5f7d1926e04e5e68160a5a1e
OPTANWAR2001 <optanwar10@gmail.com>
2024-05-12 23:17:12 +0530

    add home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <div className='logo'>
-    
+      <div className="logo">
+        <h1>Home page</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
